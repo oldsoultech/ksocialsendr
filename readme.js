27c94a643c2e63f6d1d113cf1ecba416ac23af4b
oldsoultech <oldsoultechy1@gmail.com>
2025-01-24 10:51:16 -0500

    Create readme.js

---
+++ readme.js
@@ -0,0 +1,10 @@
+/*
+ * Hey everyone! 
+ * 
+ * So this is a repository for a webhook sender that will also receive messages.
+ * It's still in alpha development, btw. 
+ * 
+ * The receiving feature doesn't work and is a placeholder.
+ * 
+ * Anyways, that's all. :D
+ */
